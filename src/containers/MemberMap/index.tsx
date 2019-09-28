c67f13595564484b8ec3541bd2d971cbc0904287
MY1112 <553570934@qq.com>
2019-09-28 18:48:44 +0800

    fix: 修改地图标注点显示

--- src/containers/MemberMap/index.tsx
+++ src/containers/MemberMap/index.tsx
@@ -5,15 +5,31 @@ interface IProps {}
 
 interface IState {}
 
+const markIcon = require('../../assets/imgs/map/bonsai.png')
 
 class MemberMap extends Component<IProps, IState> {
-
-    componentDidMount() {
-        const BMap = window.BMap
-        const mp = new BMap.Map('memberMap_content');  
-        mp.centerAndZoom(new BMap.Point(116.404, 39.915), 15);
-        mp.enableScrollWheelZoom(true);
+    private BMap: any
+    constructor(props: IProps) {
+        super(props)
+        this.BMap = window.BMap
     }
+    componentDidMount() {
+        const mp = new this.BMap.Map('memberMap_content');  
+        const point = new this.BMap.Point(116.404, 39.915)
+        mp.centerAndZoom(point, 15);
+        mp.enableScrollWheelZoom(true);
+        mp.addControl(new this.BMap.NavigationControl())
+        const marker = new this.BMap.Marker(point);        // 创建标注    
+        mp.addOverlay(marker);
+    }
+
+    // private addMarker = (mp: any, point: any) => {  // 创建图标对象   
+    //     const myIcon = new this.BMap.Icon(markIcon, new this.BMap.Size(120, 120), {       
+    //         anchor: new this.BMap.Size(10, 25)  
+    //     });      
+    //     const marker = new this.BMap.Marker(point, {icon: myIcon});    
+    //     mp.addOverlay(marker);    
+    // }   
 
     render() {
         return(
